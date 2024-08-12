acf4cb56f5d6a388f6ef6bb5431658845fa1cbed
AJPadillo <98108563+AJPadillo@users.noreply.github.com>
2024-08-12 19:45:05 +0000

    actualizado 3

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -92,6 +92,26 @@ const getState = ({ getStore, getActions, setStore }) => {
 					console.log("Error loading message from backend", error)
 				}
 			},
+
+			// Use getActions to call a function within a fuction
+			// exampleFunction: () => {
+			// 	getActions().changeColor(0, "green");
+			// },
+
+			// changeColor: (index, color) => {
+			// 	//get the store
+			// 	const store = getStore();
+
+			// 	//we have to loop the entire demo array to look for the respective index
+			// 	//and change its color
+			// 	const demo = store.demo.map((elm, i) => {
+			// 		if (i === index) elm.background = color;
+			// 		return elm;
+			// 	});
+
+			// 	//reset the global store
+			// 	setStore({ demo: demo });
+			// }
 		}
 	};
 };
